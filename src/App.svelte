7a130f3d1e82af388f05eb8e56cd987acec35764
Nathan Jang <nathanjang12@gmail.com>
2024-05-07 12:28:51 +0800

    Sigma Changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
-  import img from "./assets/drake pie.png"
+  import img from "./assets/drake pie.png";
+  import * as THREE from "three";
 
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  );
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+  animate();
 </script>
 
-<h1> Hello! this site is for Nathan, Lucas, Chun Yin, Joshua humanities project! </h1>
-<img src={img}/>
+<h1>
+  Hello! this site is for Nathan, Lucas, Chun Yin, Joshua humanities project!
+</h1>
